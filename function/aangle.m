3c2cb8c559136075a4324ba95389b771a7b63779
Alessandro Barbieri <lssndrbarbieri@gmail.com>
2018-09-06 22:31:22 +0200

    angle -> aangle
    angle è in octave

--- function/aangle.m
+++ function/aangle.m
@@ -1,6 +1,4 @@
-
-
-function alfa=angle(P1,P2,P3)
+function alfa=aangle(P1,P2,P3)
 
 %ANGLE (Spacelib) :  Angle between 3 points.
 %
@@ -8,12 +6,11 @@ function alfa=angle(P1,P2,P3)
 % between vectors(P1-P2) and (P3-P2).
 % Usage:
 %
-%			alfa=angle(P1,P2,P3)			
+%			alfa=aangle(P1,P2,P3)			
 %
 %
 % © G.Legnani, C. Moiola 1998; adapted from: G.Legnani and R.Adamini 1993
 %___________________________________________________________________________
-
 
 a=vect(P1,P2);
 a=unitv(a);
@@ -24,6 +21,3 @@ b=unitv(b);
 dot3(a,b);
 
 alfa=acos(dot3(a,b));
-
-
-
